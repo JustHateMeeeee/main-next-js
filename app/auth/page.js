a5ae9468046de073818d6f7a25a6a8770a6f2a6b
JustHateMeeeee <Artem10popov@mail.ru>
2023-06-06 22:17:43 +0500

    new http

--- app/auth/page.js
+++ app/auth/page.js
@@ -1,28 +1,29 @@
 import 'app/layout.js'
+import styles from "app/page.module.css"
 export default function auth() {
     return   <body> 
                 <main>
-                    <div class="frame">
-                        <div class="frame_reg">
+                    <div className={styles.frame}>
+                        <div className={styles.frame_reg}>
                             <form id="forma" class="form" action="<адрес 1>">
-                                <div>
-                                    <h1 class="forma_text">Форма входа</h1>
-                                </div>
-                            <div class="email">
-                                <input class="pole" type="email" maxlength="30" name="Email"
+                            <div>
+                                <h1 className={styles.forma_text}>Форма входа</h1>
+                            </div>
+                            <div className={styles.email}>
+                                <input className={styles.pole} type="email" maxlength="30" name="Email"
                                 placeholder="Login" title="Поле для Email" form="forma"></input>
                             </div>
 
-                            <div class="password">
-                                <input class="pole" type="password" maxlength="20" name="Password"
+                            <div className={styles.password}>
+                                <input className={styles.pole} type="password" maxlength="20" name="Password"
                                 placeholder="Password" pattern="[A-Za-z0-9]{10,}" title="Поле для пароля"
                                 autocomplete="on" form="forma"></input>
                             </div>
 
-                            <div class="reg_btn">
-                                <input class="reg" id="btn" type="submit" value="Зарегестрироваться" formaction="<адрес 1>" form="forma"></input>
+                            <div className={styles.reg_btn}>
+                                <input className={styles.reg} id="btn" type="submit" value="Зарегестрироваться" formaction="http://localhost:3000/comments" form="forma"></input>
                             </div>
-                        </form>
+                            </form>
                     </div>
                 </div>
             </main>
--- app/comments/page.js
+++ app/comments/page.js
@@ -1,49 +1,55 @@
-import 'app/layout.js'
-export default function () {
+'use client';
+import 'app/globals.css'
+import styles from "app/page.module.css"
+import { useState, useEffect } from 'react';
+
+const Contacts = () => {
+    const [contacts, setContacts] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('http://localhost:8080/getAllNotes');
+            const data = await response.json();
+            setContacts(data);
+        }
+        fetchData();
+    }, []);
+
     return   <body>
-    <main>
-        <div class="Frame_page">
-            <div class="Frame">
-                <div class="Comment_frame">
-                    <form class="comment_form" action="<адрес1>">
-                        <textarea name="coment" id="coment" 
-                        maxlength="20023123" placeholder="Подсказывающий текст"
+    <main className={styles.body}>
+        <div className={styles.Frame_page}>
+            <div className={styles.Frame}>
+                <div className={styles.Comment_frame}>
+                    <form className={styles.comment_form} action="https://jsonplaceholder.typicode.com/users">
+                        <textarea name="coment" className={styles.coment} 
+                        maxlength="80" placeholder="Подсказывающий текст"
                         autocomplete="off"></textarea>
-                        <div class="otprav">
-                        <input class="otprav_btn" id="btn" type="submit" name="Btn_form_otprav"
-                        value="Отпаравить" formaction="<адрес1>"></input>
+                        <div className={styles.otprav}>
+                        <input className={styles.otprav_btn} id="btn" type="submit" name="Btn_form_otprav"
+                        value="Отпаравить" formaction="https://jsonplaceholder.typicode.com/users"></input>
                         </div>
                     </form>
                 </div>
-                <div class="comment_pole">
+                <div className={styles.comment_pole}>
                     <form action="User_coment">
-                        <ul class="comment_field">
-                            <li>
-                                <div class="Commentari">
-                                    <div class="sam_com">
-                                        <p>Lorem ipsum dolor sit, amet consectetur 
-                                        adipisicing elit. Explicabo expedita, at
-                                        vitae sed aliquam voluptates debitis vol
-                                        uptas incidunt cupiditate sit maiores! As
-                                        sumenda, natus numquam nesciunt 
-                                        facere laudantium officiis asperiores accusantium.
-                                        </p>
-                                    </div>                                    
+                        <ul className={styles.comment_field}>
+                        {contacts && contacts.map(({ id, username, email }) => (       
+                            <li key={id}>
+                                <div className={styles.Commentari}>
+                                    <div className={styles.sam_com}>
+                                    {username}({email})
+                                    </div>
+                                    <button className={styles.close_btn} type="submit" name="close_btn">
+                                        <svg  width="26px" height="26px" viewBox="-3.2 -3.2 38.40 38.40" transform="matrix(1, 0, 0, 1, 0, 0)rotate(0)">
+                                            <path d="M18.8,16l5.5-5.5c0.8-0.8,0.8-2,0-2.8l0,0C24,7.3,23.5,7,23,7c-0.5,0-1,0.2-1.4,0.6L16,13.2l-5.5-5.5 c-0.8-0.8-2.1-0.8-2.8,
+                                            0C7.3,8,7,8.5,7,9.1s0.2,1,0.6,1.4l5.5,5.5l-5.5,5.5C7.3,21.9,7,22.4,7,23c0,0.5,
+                                            0.2,1,0.6,1.4 C8,24.8,8.5,25,9,25c0.5,0,1-0.2,1.4-0.6l5.5-5.5l5.5,5.5c0.8,0.8,2.1,0.8,2.8,0c0.8-0.8,0.8-2.1,0-2.8L18.8,16z">
+                                            </path>
+                                        </svg>
+                                    </button>                                   
                                 </div>
                             </li>
-                            <li>
-                                <div class="Commentari">
-                                    <div class="sam_com">
-                                        <p>Lorem ipsum dolor sit, amet consectetur 
-                                        adipisicing elit. Explicabo expedita, at
-                                        vitae sed aliquam voluptates debitis vol
-                                        uptas incidunt cupiditate sit maiores! As
-                                        sumenda, natus numquam nesciunt 
-                                        facere laudantium officiis asperiores accusantium.
-                                        </p>
-                                    </div>                                    
-                                </div>
-                            </li>
+                            ))}
                         </ul>
                     </form>
                 </div>
@@ -53,3 +59,5 @@ export default function () {
 </body>
 
 }
+
+export default Contacts;
